120d6eac7ccfcad3000e1917761ffe529e22975c
Faizan Ali <fa686319@gmail.com>
2023-02-12 18:59:12 +0500

    fixed pickle path

--- src/core/config.py
+++ src/core/config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
   DB_PATH: str = f"{os.getcwd()}/FaceAI"
-  PICKLE_PATH: str = f"{os.getcwd}/Lisences.pickle"
+  PICKLE_PATH: str = f"{os.getcwd()}/Lisences.pickle"
 
 conf = Config()
