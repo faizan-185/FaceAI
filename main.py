c95013bd0f50cb7db0ba73990596c6184ca57031
Faizan Ali <fa686319@gmail.com>
2023-02-25 21:28:11 +0500

    reformat

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from src.crud import readLisences, isLocked, matchLisence, removeLisence, update
 from src.core.config import conf
 from src.core.db import session, settings_table
 
-
 if __name__ == "__main__":
-  if not isLocked(session=session, table=settings_table):
-    print("ok")
-  else:
-    lisence_key = input("Enter Lisence Key: ")
-    lisences = readLisences(conf.LISENCES_PATH)
-    result = matchLisence(lisences=lisences, my_lisence=lisence_key)
-    if result:
-      removeLisence(path=conf.LISENCES_PATH, lisences=lisences, my_lisence="abcd")
-      updateData(session=session, table=settings_table, key="Locked", value="No")
-      insertData(session=session, table=settings_table, data=("validity time", "one year"))
-
+    if not isLocked(session=session, table=settings_table):
+        print("ok")
     else:
-      print("Incorrect Lisence Key")
+        license_key = input("Enter License Key: ")
+        licenses = readLisences(conf.LISENCES_PATH)
+        result = matchLisence(lisences=licenses, my_lisence=license_key)
+        if result:
+            removeLisence(path=conf.LISENCES_PATH, lisences=licenses, my_lisence="abcd")
+            updateData(session=session, table=settings_table, key="Locked", value="No")
+            insertData(session=session, table=settings_table, data=("validity time", "one year"))
+
+        else:
+            print("Incorrect License Key")
