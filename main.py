c4e33862daf175c8f3f4833db32a503e254a3bca
aali08 <aali08i2cinc@gmail.com>
2023-03-05 18:13:48 +0500

    change for get Peocessor Batch No and Sr no Logic and work.

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ from src.crud import readLisences, isLocked, matchLisence, removeLisence, update
 from src.core.config import conf
 from src.core.db import session, settings_table
 from src.similarity import runner
+import platform, subprocess, re
+
+
+def getProcessor_batch_serial_info(cmd):
+
+    if platform.system() == "Windows":
+        completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+        if completed.stdout != "":
+            info = completed.stdout
+            info = info.strip()
+            info = info.decode('utf-8')
+            infoObj = dict(item.split(":") for item in re.sub('\s+', '', re.sub('\n', ';', info)).split(";"))
+            return infoObj
+
 
 if __name__ == "__main__":
+    print(getProcessor_batch_serial_info("p"))
+
+
     if not isLocked(session=session, table=settings_table):
         runner()
     else:
